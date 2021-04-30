264336486ce770b6466c0eb6ecd42b0fb2510dda
nikhilkomari <nkomari@uwo.ca>
2021-04-30 18:36:55 -0400

    initial commit

---
+++ base/urls/user_urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from base.views import user_views as views
+
+
+urlpatterns = [
+    path('', views.get_users, name='users'),
+
+    path('register/', views.register_user, name='register'),
+    path('login/', views.MyTokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
+
+    path('profile/', views.get_user_profile, name='user-profile'),
+    path('profile/update/', views.update_user_profile, name='user-profile-update'),
+
+    path('<str:pk>/', views.get_user_by_id, name='user'),
+
+    path('update/<str:pk>/', views.update_user, name='user-update'),
+
+    path('delete/<str:pk>/', views.delete_user, name='user-delete'),
+]
